7bc7cdf756c7e93ff982ee2549d441b38b50f73f
Jean-Eric Pierre-Louis <55325769+Nduriel@users.noreply.github.com>
2020-12-16 16:25:50 -0500

    organization and pics

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -1,19 +1,89 @@
-import React from "react";
-import { Jumbotron, Container, Row, Col, Media } from "reactstrap";
+import React, { useState } from "react";
+import { carouselPic } from "./common/carouselPics";
 import { FadeTransform } from "react-animation-components";
+import {
+  Jumbotron,
+  Container,
+  Row,
+  Col,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+} from "reactstrap";
 
 function Home() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex =
+      activeIndex === carouselPic.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex =
+      activeIndex === 0 ? carouselPic.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = carouselPic.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+      </CarouselItem>
+    );
+  });
   return (
     <Jumbotron id="main" fluid>
       <Container fluid>
         <Row style={{ alignItems: "center" }}>
-          <Col lg={5} md={12} sm={12} className="text-center">
+          <Col
+            lg={{ size: 5, offset: 1 }}
+            md={{ offset: 1 }}
+            sm={{ size: 10, offset: 1 }}
+            xs={12}
+            className="text-center"
+          >
             <FadeTransform
               enterTransform="translateX(0px)"
               exitTransform="translateX(-120px)"
               in
             >
-              <Media id="homePic" object src={"./images/Me1.jpg"} alt="Me" />
+              <Carousel
+                activeIndex={activeIndex}
+                next={next}
+                previous={previous}
+              >
+                <CarouselIndicators
+                  items={carouselPic}
+                  activeIndex={activeIndex}
+                  onClickHandler={goToIndex}
+                />
+                {slides}
+                <CarouselControl
+                  direction="prev"
+                  directionText="Previous"
+                  onClickHandler={previous}
+                />
+                <CarouselControl
+                  direction="next"
+                  directionText="Next"
+                  onClickHandler={next}
+                />
+              </Carousel>
             </FadeTransform>
           </Col>
           <Col lg={6} md={12} sm={12}>
---
+++ src/Components/common/carouselPics.js
@@ -0,0 +1,22 @@
+export const carouselPic = [
+  {
+    altText: "1st Pic",
+    src: "../images/Me.jpg",
+  },
+  {
+    altText: "2nd Pic",
+    src: "../images/honeymoon.jpg",
+  },
+  {
+    altText: "3rd Pic",
+    src: "../images/usmcme2.jpg",
+  },
+  {
+    altText: "4thd Pic",
+    src: "../images/danyWedding.jpg",
+  },
+  {
+    altText: "4thd Pic",
+    src: "../images/Me2.jpg",
+  },
+];
